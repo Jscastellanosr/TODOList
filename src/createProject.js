9bdae9aff2d417e73dd414b771a922395e8888dc
Castel <jscastellanosr94@gmail.com>
2023-01-29 09:01:37 -0500

    adding initial layout and createProject methods

--- src/createProject.js
+++ src/createProject.js
@@ -1,17 +1,37 @@
-
-
-
-let projects = [];
-
+const projectContainer = document.querySelector('.projectContainer')
+const todoContainer = document.querySelector('.todoContainer')
 export default class project {
     constructor(name) {
-        this.name = name
+        this.name = name,
+        this.todos =[]
     }
 
-    addProject () {
-        projects.push(this)
+    static addTODO (object, todo) {
+        object.todos.push(todo)
     }
 
+    static addProject (object) {
+       localStorage.setItem(`${object.name}`, JSON.stringify(object))
+    }
+
+    static render(object) {
+        const proj = document.createElement('li');
+        proj.textContent = object.name;
+        proj.addEventListener('click', ()=> {
+            this.renderTodos(object);
+        })
+        projectContainer.appendChild(proj);
+    }
+
+    static renderTodos (object) {
+        todoContainer.innerHTML =""
+
+        object.todos.forEach(element => {
+            const todo = document.createElement('div')
+            todo.textContent = element;
+            todoContainer.appendChild(todo);
+        });
+
+        console.log('ok nice')
+    }
 }
-
-export {projects}
--- src/index.js
+++ src/index.js
@@ -1,19 +1,56 @@
 import project from './createProject.js'
-import { projects } from './createProject.js'
+
+const addProj = document.querySelector('.addProject')
+const formWindow = document.querySelector('.formWindow')
+const subm = document.querySelector('#subm')
 
 
 window.addEventListener('DOMContentLoaded', ()=>{
 
     /* LOAD STORAGE CONTENT */
-    if(localStorage.getItem('default')){
-        console.log('yes')
-    } else{
 
+    /* check if default exists */
+    if(localStorage.getItem('Default')){
+        console.log('yes')
+        console.log(JSON.parse(localStorage.getItem('Default')).name)
+    } else{
         const defProj = new project('Default')
-        defProj.addProject()
-        console.log(projects)
+
+        /* ADDING TODOS FOR TESTING */
+
+        project.addTODO(defProj, 'do this and do that')
+        project.addTODO(defProj, 'now do this and do that')
+        project.addTODO(defProj, 'then do this and do that')
+        project.addTODO(defProj, 'after do this and do that')
+        
+        project.addProject(defProj);
         console.log('no')
+
     }
 
+    const project1 = new project('Project1')
+    project.addProject(project1)
 
+    console.log(localStorage)
+    /*RETRIEVE LOCALGROUP OBJECTS */
+    const projectArray = [];
+    console.log('...........')
+    for(let i = localStorage.length - 1; i >= 0 ; i--){
+        projectArray.push(JSON.parse(localStorage.getItem(localStorage.key(i))))
+    }
+
+    /* RENDER ARRAYS */
+
+    for(let i = 0; i < projectArray.length; i++) {
+       project.render(projectArray[i])
+    }
+
+})
+
+addProj.addEventListener('click', ()=>{
+    formWindow.classList.toggle('inactive')
+})
+
+subm.addEventListener('click', ()=>{
+    formWindow.classList.toggle('inactive')
 })
