e3ee04aeeb3cbb13beea74b355b743fbff30493e
Castel <jscastellanosr94@gmail.com>
2023-01-29 12:14:42 -0500

    add TODO module

---
+++ src/createTodo.js
@@ -0,0 +1,14 @@
+
+
+
+export default class TODO {
+
+    constructor(task, date, description = "", priority){
+        this.task = task,
+        this.date = date,
+        this.description = description,
+        this.priority = priority
+    }
+
+
+}
